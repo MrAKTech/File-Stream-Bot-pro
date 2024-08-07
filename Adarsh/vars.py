88104a1580baa860e23a42d1532fa69853f79ea7
MrAK Techz <l20cs001@vvcoe.org>
2024-08-07 22:30:23 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,4 +1,3 @@
-# (c) adarsh-goel
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -7,42 +6,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '23990433'))
-    API_HASH = str(getenv('API_HASH', 'e6c4b6ee1933711bc4da9d7d17e1eb20'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','6119758281:AAFNCl4gzd-1w957bRKEhjb6kW1JnaDZm3U'))
-    name = str(getenv('name', 'filetolinkbot'))
+    API_ID = int(getenv('API_ID', '22225617'))
+    API_HASH = str(getenv('API_HASH', 'ef16f7597376f1689663304c954e4493'))
+    PICS = (environ.get('PICS','https://graph.org/file/8cd764fbdf3ccd34abe22.jpg')).split()
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7242317141:AAGAIjdTlThKtArXmB0bD7bDMmus10Ur6G0'))
+    name = str(getenv('name', 'NobitaStreamBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001682397310'))
-    PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '16.171.19.76:8080'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002053122232'))
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "5821871362").split()}
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6072149828").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@sankar2k02'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'I_Am_MrAK'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
-
+    
     else:
         ON_HEROKU = False
-    FQDN = (
-        str(getenv('FQDN', '16.171.19.76:8080'))
-        if not ON_HEROKU or getenv('FQDN', '16.171.19.76:8080')
-        else f'{APP_NAME}.herokuapp.com'
-    )
-    HAS_SSL=bool(getenv('HAS_SSL',False))
-    URL = f"https://{FQDN}/" if HAS_SSL else f"http://{FQDN}/"
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
-    BANNED_CHANNELS = list(
-        {
-            int(x)
-            for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()
-        }
-    ) 
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL', True))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://akmonsterprogrammer:S.Aruna1155182089@filetolink.erqitim.mongodb.net/?retryWrites=true&w=majority&appName=filetolink'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'MrAK_LinkZz'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
